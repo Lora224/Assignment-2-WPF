afabfee021e919f1397b90f23ac0a1dc18d3caac
Kultz NG <163091790+kultzng@users.noreply.github.com>
2024-10-22 16:37:55 +1100

    Update Pet.cs

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -16,28 +16,31 @@ namespace Assignment_2_WPF.Models
         public string PetName { get; set; }
         public int UserId { get; set; }
         public string Breed { get; set; }
-        public DateTime dob { get; set; }
+        public DateTime Dob { get; set; }
         public int Weight { get; set; }
         public List<Activity> activities;
         public List<Schedule> schedules;
-                              
 
         //constructor of class Pet, with petId generated automatically
         public Pet(int userId, string petName, string breed, DateTime dob, int weight)
         {
             Random random = new Random();
-            petId = random.Next(100000, 999999);
+            this.petId = random.Next(100000, 999999);
             this.userId = userId;
             this.petName = petName;
             this.breed = breed;
-            this.dob = dob;
+            this.Dob = dob;
             this.weight = weight;
             activities = new List<Activity>();
             schedules = new List<Schedule>();
+
+            // Initialize properties to avoid CS8618 error
+            PetName = petName;
+            Breed = breed;
         }
 
         // using Entity framework to query all pet from table pet in PetApp.db, and then count the number of pets
-        public int countPet()
+        public static int countPet()
         {
             using (var context = new AppDbContext())
             {
@@ -45,7 +48,7 @@ namespace Assignment_2_WPF.Models
                 return pets.Count;
             }
         }
-        
+
 
         // if pets.Count ==0, show petMenuNoPet, else show petMenu
         public void petCheck()
@@ -68,7 +71,7 @@ namespace Assignment_2_WPF.Models
             Console.WriteLine("1. Add new pet");
             Console.WriteLine("2. Back");
             Console.WriteLine("Please enter your choice: "); // in wpf this should be a button to choose the option
-            string choice = Console.ReadLine();
+            string choice = Console.ReadLine();  
             switch (choice)
             {
                 case "1":
@@ -139,7 +142,7 @@ namespace Assignment_2_WPF.Models
                     //Console.WriteLine("Pet ID: " + pet.PetId);
                     Console.WriteLine("Pet Name: " + pet.PetName);
                     Console.WriteLine("Breed: " + pet.Breed);
-                    Console.WriteLine("DOB: " + pet.dob);
+                    Console.WriteLine("DOB: " + pet.Dob);
                     Console.WriteLine("Weight: " + pet.Weight);
                     Console.WriteLine();
                 }
@@ -158,7 +161,7 @@ namespace Assignment_2_WPF.Models
                 {
                     Console.WriteLine("Pet Name: " + pet.PetName);
                     Console.WriteLine("Breed: " + pet.Breed);
-                    Console.WriteLine("DOB: " + pet.dob);
+                    Console.WriteLine("DOB: " + pet.Dob);
                     Console.WriteLine("Weight: " + pet.Weight);
                     // activity list
                     // schedule list - toString() in schedule class
@@ -172,24 +175,24 @@ namespace Assignment_2_WPF.Models
             Console.WriteLine("Press any key to return to the pet menu");
             Console.ReadKey();
         }
-        
+
         public void addNewPet()
         {
             Console.WriteLine("Add pet ");
-            string ?nameTemp;
+            string? nameTemp;
             //add pet with details filled in forms to construct a new pet object? and write in to database
             Console.Write("name");
             nameTemp = Console.ReadLine();
             DateTime date = new DateTime(2000, 2, 24); //test data for date
-            Pet newPet = new Pet(userId, nameTemp,"Dog",date,50);
-      /*    using (var context = new AppDbContext())
-            {
-                context.Pets.Add(newPet);
-                context.SaveChanges();
-            }
+            Pet newPet = new Pet(userId, nameTemp, "Dog", date, 50);
+            /*    using (var context = new AppDbContext())
+                  {
+                      context.Pets.Add(newPet);
+                      context.SaveChanges();
+                  }
 
-        */
-            
+              */
+
         }
         public void editPetDetails()
         {
@@ -202,3 +205,5 @@ namespace Assignment_2_WPF.Models
 
     }
 }
+    
+
