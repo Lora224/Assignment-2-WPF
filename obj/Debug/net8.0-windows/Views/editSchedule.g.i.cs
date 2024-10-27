28d0d169b22f7353f9dc59e45c5f0516bfe1b1db
Muze Lyu <35753187+Lora224@users.noreply.github.com>
2024-10-27 15:57:33 +1100

    petview addnewpet activity appearence update

--- obj/Debug/net8.0-windows/Views/editSchedule.g.i.cs
+++ obj/Debug/net8.0-windows/Views/editSchedule.g.i.cs
@@ -54,7 +54,7 @@ namespace Assignment_2_WPF.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Assignment 2 WPF;V1.0.0.0;component/views/editschedule.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Assignment 2 WPF;component/views/editschedule.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\editSchedule.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
